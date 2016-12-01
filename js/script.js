0f8f5df979cffce10eeff1262e5af9b27c4f5fb2
Clemente Garcia-Huerta <cgarciahuerta@horizon.csueastbay.edu>
2016-12-01 10:49:10 -0800

    Add files via upload
    This is my portfolio as of December 1 of 2016.

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(".navbar-fixed-top").autoHidingNavbar(function(){
+	$(scroll).hide("showOnUpscroll");
+});
+// disable auto-hide
+disableAutohide: true,
+
+// shows up when scrolling the page upwards
+showOnUpscroll: true,
+
+// shows up when scroll reaches the page's end.
+showOnBottom: true,
+
+// "auto" means the navbar height
+hideOffset: 'auto', 
+
+// The duration of the show and hide animations
+animationDuration: 200
+
+
+
+
+
+
+
